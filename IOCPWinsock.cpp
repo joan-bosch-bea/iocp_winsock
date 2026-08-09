4c505ec3476b4c89a8da2ed3a88bfadb79d886ec
Joan Bosch <bosch.bea.joan@gmail.com>
2026-08-09 09:30:15 +0200

    codi inicial del servidor, inicia winsock, crea listening socket associat a l'adressa i inicia l'escolta

---
+++ IOCPWinsock.cpp
@@ -0,0 +1,55 @@
+#ifndef WIN32_LEAN_AND_MEAN
+	#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct SERVER_CONTEXT {
+	HANDLE hCompletionPort = nullptr;
+	SOCKET listeningSocket = INVALID_SOCKET;
+};
+
+int main() {
+	WSADATA wsaData;
+	sockaddr_in serverAddress{};
+	SYSTEM_INFO systemInfo;
+	DWORD workerCount;
+	vector<HANDLE> hWorkersVector;
+	SERVER_CONTEXT serverContext;
+
+	//inicialitza winsock
+	if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		cout << "Error incialitzant llibreria Winsock v2.2" << endl;
+		return 1;
+	}
+
+	//listening socket
+	serverContext.listeningSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
+	if(serverContext.listeningSocket == INVALID_SOCKET) {
+		cout << "Error en crear el listening socket" << endl;
+		WSACleanup();
+		return 1;
+	}
+
+	//associar el listeningSocket a l'adressa
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	if(bind(serverContext.listeningSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+		closesocket(serverContext.listeningSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	//iniciar l'escolta
+	if(listen(serverContext.listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
+		cout << "Error en iniciar escolta" << endl;
+		closesocket(serverContext.listeningSocket);
+		WSACleanup();
+		return 1;
+	}
+}
